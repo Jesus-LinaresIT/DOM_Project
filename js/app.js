2a295fde0753f010fca57c65302c62409d2646f0
JesusLinares01 <alberto.salinas@asesoriait.com>
2021-09-15 18:09:40 -0600

    Adding UI manipulating DOM methods with JavaScript in the 'AddTask' block

---
+++ js/app.js
@@ -0,0 +1,45 @@
+// Define UI vars
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const taskList = document.querySelector('.collection');
+const filter = document.querySelector('#filter');
+const clearBtn = document.querySelector('.clear-tasks');
+
+
+// Load all event listeners
+loadEventListeners();
+// Load all event listeners
+function loadEventListeners() {
+   // Add task event
+   form.addEventListener('submit', addTask);
+}
+
+
+// Add Task
+function addTask(e){
+   if (taskInput.value !== ''){
+      // Create li element
+      const li = document.createElement('li');
+      // Add class
+      li.className = 'collection-item';
+      // Create text node and append to li
+      li.appendChild(document.createTextNode(taskInput.value));
+      // Create new link element
+      const link = document.createElement('a');
+      // Add class
+      link.className='delete-item secondary-content';
+      // Add icon HTML
+      link.innerHTML='<i class="fas fa-trash"></i>';
+      // Append the link to li
+      li.appendChild(link);
+      // Append li to ul
+      taskList.appendChild(li);
+      // Clear input
+      taskInput.value = '';
+   }else{
+      alert('Please add task to collection');
+   }
+
+
+   e.preventDefault();
+}
