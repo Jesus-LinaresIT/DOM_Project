1c71b1758a6b0bad6fa5f686fde91ec5e95faef5
JesusLinares01 <alberto.salinas@asesoriait.com>
2021-09-20 11:54:35 -0600

    Remove an item and clear all item from local storage'

--- js/app.js
+++ js/app.js
@@ -93,8 +93,30 @@ function removeTask(e){
    if(e.target.parentElement.classList.contains('delete-item')){
       if(confirm('Are you sure you want to remove task?')){
          e.target.parentElement.parentElement.remove();
+
+         // Remove from LS
+         removeTaskFromLocalStore(e.target.parentElement.parentElement);
       }
    }
+}
+
+
+// Remove from localStorage
+function removeTaskFromLocalStore(taskItem){
+   let tasks;
+   if(localStorage.getItem('tasks') === null){
+      tasks = [];
+   }else{
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+   }
+
+   tasks.forEach(function(task, index){
+      if(taskItem.textContent === task){
+         tasks.splice(index, 1);
+      }
+   });
+
+   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 
@@ -104,7 +126,15 @@ function clearTasks(){
       while (taskList.firstChild) {
          taskList.removeChild(taskList.firstChild);
       }
+      // Clear task from LS
+      clearTaskFromLocalStore();
    }
+}
+
+
+// Clear tasks form from localStorage
+function clearTaskFromLocalStore(){
+   localStorage.clear();
 }
 
 
