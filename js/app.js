e827b5a9364472cd5ff7f0dc39cdc303ef1cde85
JesusLinares01 <alberto.salinas@asesoriait.com>
2021-09-20 11:35:55 -0600

    Add function to persist to local storage 'Store in local storage'

--- js/app.js
+++ js/app.js
@@ -1,103 +1,97 @@
 // Define UI vars
 const form = document.querySelector('#task-form');
 const taskInput = document.querySelector('#task');
-const taskList = document.querySelector('.collection');
 const filter = document.querySelector('#filter');
-const clearBtn= document.querySelector('.clear-tasks');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
 
 
 // Load all event listeners
 loadEventListeners();
+
 // Load all event listeners
-function loadEventListeners() {
-
-   // Add task event
+function loadEventListeners(){
+   // Add task
    form.addEventListener('submit', addTask);
-
-   // Remove a task list
+   // Remove a task
    taskList.addEventListener('click', removeTask);
-
-   // Remove all task
-   clearBtn.addEventListener('click', clearTask);
-
-   // Filter
+   // Clear all task
+   clearBtn.addEventListener('click', clearTasks);
+   // Filter task
    filter.addEventListener('keyup', filterTasks);
 }
 
-
-// Add Task
+// Add task
 function addTask(e){
    if (taskInput.value !== ''){
-
-      // Create li element
       const li = document.createElement('li');
-
-      // Add class
       li.className = 'collection-item';
-
-      // Create text node and append to li
       li.appendChild(document.createTextNode(taskInput.value));
 
-      // Create new link element
       const link = document.createElement('a');
+      link.className = 'delete-item secondary-content';
+      link.innerHTML = "<i class='fa fa-trash'></i>";
 
-      // Add class
-      link.className ='delete-item secondary-content';
-
-      // Add icon HTML
-      link.innerHTML = '<i class="fas fa-trash"></i>';
-
-      // Append the link to li
       li.appendChild(link);
-
-      // Append li to ul
       taskList.appendChild(li);
 
-      // Clear input
+      // Store in LS
+      storeTaskLocalStore(taskInput.value);
+
       taskInput.value = '';
    }else{
-      alert('Please add task to collection');
+      alert('Please add task');
    }
 
    e.preventDefault();
 }
 
+// Store task
+function storeTaskLocalStore(task){
+   let tasks;
+   if(localStorage.getItem('tasks') === null){
+      tasks = [];
+   }else{
+      tasks = JSON.parse(localStorage.getItem('tasks'))
+   }
 
-
-// Remove Task
-function removeTask(e){
-   if(e.target.parentElement.classList.contains
-      ('delete-item')){
-         if(confirm('Are you sure you want to remove task?')){
-            e.target.parentElement.parentElement.remove();
-         }
-      }
+   tasks.push(task);
+   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 
-// Clear Tasks
-function clearTask(e){
-   // Remove all tasks from collection
-   while(taskList.firstChild){
-      taskList.removeChild(taskList.firstChild);
+// Remove a task
+function removeTask(e){
+   if(e.target.parentElement.classList.contains('delete-item')){
+      if(confirm('Are you sure you want to remove task?')){
+         e.target.parentElement.parentElement.remove()
+      }
    }
 }
 
 
-// Filter Tasks
-function filterTasks(e){
-
-   const text = e.target.value.toLowerCase();
-
-   document.querySelectorAll('.collection-item').forEach(
-      function(task){
-         const item = task.firstChild.textContent;
-         if(item.toLowerCase().indexOf(text) !== -1){
-            task.style.display = 'block';
-         }else{
-            task.style.display = 'none';
-         }
-      });
+// Clear all task
+function clearTasks(){
+   if(confirm('Are you sure you want to remove all tasks?')){
+      while (taskList.firstChild) {
+         taskList.removeChild(taskList.firstChild);
+      }
+   }
 }
 
 
+// Filter tasks
+function filterTasks(e){
+   const text = e.target.value.toLowerCase();
+
+   document.querySelectorAll('.collection-item').forEach(
+      function(task) {
+      const item = task.firstChild.textContent;
+
+      if (item.toLowerCase().indexOf(text) !== -1) {
+         task.style.display = 'block';
+      }else{
+         task.style.display = 'none';
+      }
+   });
+}
